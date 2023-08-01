1f87f260b7e1878600206773595794c1755db66e
Ana Lívia <analiviamgarbin@gmail.com>
2023-08-01 10:53:53 -0300

    main para testes

--- main.py
+++ main.py
@@ -5,46 +5,6 @@ import time
 import tkinter as tk  # biblioteca de interface grafica
 
 
-# auto atualização
-# funcao de atualizacao a partir do repositorio
-# fonte: repositorio de parametro para atualizacao
-# update: arquivo a ser atualizado
-def update_git(fonte, main_cod):
-    try:
-        # se o repositorio nao existe na maquina, tentativa de clonar
-        repo = git.Repo.fonte(fonte, "temp_fonte", branch="main")
-    except git.exc.GitCommandError:
-        # caso o repositorio ja exista localmente, realizar um pull para atualizar
-        repo = git.Repo("temp_fonte")
-        repo.remotes.origin.pull()
-
-    # atualiza o arquivo alvo com a versao mais recente
-    # abre o arquivo main_cod em modo de escrita 'w'
-    with open(main_cod, "w") as target:
-        # le o conteudo do arquivo temporario e escreve no main_cod (target)
-        with open(f"temp_fonte/{main_cod}") as source:
-            target.write(source.read())
-
-    # limpa o repositorio temporario apos a atualizacao
-    subprocess.run(["rm", "-rf", "temp_fonte"])
-
-
-def update_h():
-    git_url = "https://github.com/analiviagarbin/psel-shinier-2023-iot"  # define a URL do repositorio Git e o arquivo alvo para atualizar
-    main_cod = "main.py"  # arquivo principal completo
-    update_git(git_url, main_cod)  # chama a funcao para atualizar o arquivo alvo
-
-
-if __name__ == "__main__":
-    # agendando a funcao de atualizacao para ser executada a cada 1 hora
-    schedule.every(1).hour.do(update_h)
-
-    # loop para continuar agendando e executando as tarefas agendadas
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-
 # interface
 def gerar_mensagem():
     nome = nomeD.get()  # nome recebe o get de nomeD
@@ -84,3 +44,43 @@ mensagem_pronta.pack(pady=10)
 
 # inicia o loop principal da interface grafica
 janela.mainloop()
+
+
+# auto atualização
+# funcao de atualizacao a partir do repositorio
+# fonte: repositorio de parametro para atualizacao
+# update: arquivo a ser atualizado
+def update_git(fonte, main_cod):
+    try:
+        # se o repositorio nao existe na maquina, tentativa de clonar
+        repo = git.Repo.fonte(fonte, "temp_fonte", branch="main")
+    except git.exc.GitCommandError:
+        # caso o repositorio ja exista localmente, realizar um pull para atualizar
+        repo = git.Repo("temp_fonte")
+        repo.remotes.origin.pull()
+
+    # atualiza o arquivo alvo com a versao mais recente
+    # abre o arquivo main_cod em modo de escrita 'w'
+    with open(main_cod, "w") as target:
+        # le o conteudo do arquivo temporario e escreve no main_cod (target)
+        with open(f"temp_fonte/{main_cod}") as source:
+            target.write(source.read())
+
+    # limpa o repositorio temporario apos a atualizacao
+    subprocess.run(["rm", "-rf", "temp_fonte"])
+
+
+def update_h():
+    git_url = "https://github.com/analiviagarbin/psel-shinier-2023-iot"  # define a URL do repositorio Git e o arquivo alvo para atualizar
+    main_cod = "main.py"  # arquivo principal completo
+    update_git(git_url, main_cod)  # chama a funcao para atualizar o arquivo alvo
+
+
+if __name__ == "__main__":
+    # agendando a funcao de atualizacao para ser executada a cada 1 hora
+    schedule.every(1).hour.do(update_h)
+
+    # loop para continuar agendando e executando as tarefas agendadas
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
