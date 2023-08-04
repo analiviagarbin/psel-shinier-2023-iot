86a2cd6c8a84c4c1c5b639ede479bda921eacbb6
Ana Lívia Magalhães Garbin <95146046+analiviagarbin@users.noreply.github.com>
2023-08-04 15:11:59 -0300

    alteracao para video

--- interface.py
+++ interface.py
@@ -10,11 +10,11 @@ def janela():
     # cria a janela principal "Gerar Mensagem"
     janela = tk.Tk()
     janela.title("Gerar Mensagem")
-    janela.geometry("400x100")  # tamanho da janela fixo
+    janela.geometry("400x200")  # tamanho da janela fixo
     fonte = ("Arial", 12)
 
     # texto principal
-    texto_orientacao = tk.Label(janela, text="Digite o seu nome:", font=fonte)
+    texto_orientacao = tk.Label(janela, text="Digite o MUDEI seu nome:", font=fonte)
     texto_orientacao.grid(column=0, row=0)  # posiciona o texto na janela
     texto_orientacao.pack(pady=20)
 
@@ -41,7 +41,7 @@ def janela():
 
     # posiciona a janela no centro do display
     largura_janela = 400
-    altura_janela = 100
+    altura_janela = 200
     largura_tela = janela.winfo_screenwidth()
     altura_tela = janela.winfo_screenheight()
     posicao_x = int((largura_tela - largura_janela) / 2)
@@ -55,7 +55,7 @@ def janela():
 def Aviso():
     aviso = tk.Tk()
     aviso.title("Aviso - Update")
-    aviso.geometry("400x200")
+    aviso.geometry("400x100")
     fonte = ("Arial", 12)
 
     # texto principal
@@ -83,7 +83,7 @@ def Aviso():
 
     # posiciona a aviso no centro do display
     largura_aviso = 400
-    altura_aviso = 200
+    altura_aviso = 100
     largura_tela = aviso.winfo_screenwidth()
     altura_tela = aviso.winfo_screenheight()
     posicao_x = int((largura_tela - largura_aviso) / 2)
