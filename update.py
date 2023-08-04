1cf6e5d0d42be275f0c37a822edaefcb0df205d9
Ana Lívia <analiviamgarbin@gmail.com>
2023-08-04 10:23:00 -0300

    ajuste caminhos pastas

--- update.py
+++ update.py
@@ -13,21 +13,22 @@ def UpdateInt():
         shutil.move(arq_ant, os.path.join(saves, os.path.basename(arq_ant)))
 
     # clona o repositorio do link completo
-    def CloneInt(repo_url, arq_ant, temp):
+    def CloneInt(repo_url, arq_temp_interface, arq_fim, temp):
         # clonar o repositório no diretório 'temp' e mover o arquivo 'arq_ant'
         # da pasta 'temp_clone' para a pasta principal
         repo = Repo.clone_from(repo_url, temp)
-        shutil.move(
-            arq_temp_interface, os.path.join(os.getcwd(), os.path.basename(arq_ant))
-        )
+        shutil.move(arq_temp_interface, arq_fim)
 
     def reiniciar():
         # executa o arquivo main.py novamente
-        subprocess.call(["python3", "main.py"])
+        # colocar caminho completo da main
+        subprocess.call(["python3", "./main.py"])
 
     # localizacoes dos arquivos
+    # adicionar caminho COMPLETO
     repo_url = "https://github.com/analiviagarbin/testes"
     arq_ant = "./interface.py"
+    arq_fim = "./"
     arq_temp_interface = "./temp_clone/interface.py"
     temp = "./temp_clone"
     saves = "./saves"
@@ -40,11 +41,11 @@ def UpdateInt():
         os.makedirs(saves)
 
     MoveDef(arq_ant, saves)
-    CloneInt(repo_url, arq_ant, temp)
+    CloneInt(repo_url, arq_temp_interface, arq_fim, temp)
 
     # apaga as pastas temporarias
     shutil.rmtree(saves)
-    # shutil.rmtree(temp)
+    shutil.rmtree(temp)
 
     Aviso()
     reiniciar()
